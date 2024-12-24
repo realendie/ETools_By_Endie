f0e8069d4a09f1959c0954cc377a94787483e1e4
Wyatt Johnson <116504461+realendie@users.noreply.github.com>
2024-12-24 00:38:48 -0500

    Added "convert_file" function

--- ETools YouTube Downloader/main.py
+++ ETools YouTube Downloader/main.py
@@ -1,11 +1,11 @@
 import os
-import subprocess
+import ffmpeg
 import tkinter as tk
 from tkinter import messagebox
 
 def download_video():
-    link = input("Enter YouTube Video URL: ")
-    download_location = input("Enter download location: ")
+    link = input("Enter YouTube Video URL:\n")
+    download_location = input("Enter download location:\n")
     if link and download_location:
         try:
             os.system(f'yt-dlp -f best -o "{download_location}/%(title)s.%(ext)s" "{link}" -S res:1080')
@@ -14,3 +14,17 @@ def download_video():
             messagebox.showerror("Error", f"An error occurred: {e}")           
     else:
         messagebox.showerror("Error", "Please provide a valid YouTube URL and download location.")
+
+def convert_file():
+    try:
+        finput = input('Enter the input file path:\n')
+        foutput = input('Enter the output file path:\n')
+        (
+            ffmpeg.input(finput)
+            .output(foutput)
+            .run()
+        )
+        messagebox.showinfo(message='Conversion Complete')
+        print('Conversion Complete')
+    except Exception as e:
+        messagebox.showerror(message=f'{e}')
