92cba421dc1ae87de4157b1e3eb8218bbd2440db
Wyatt Johnson <116504461+realendie@users.noreply.github.com>
2025-06-21 11:07:46 +0000

    Removed tkitner functions from program

--- ETools/main.py
+++ ETools/main.py
@@ -1,32 +1,27 @@
 import os
 import ffmpeg
-import tkinter as tk
-from tkinter import messagebox
-
 
 def download_video():
-    link = input("Enter YouTube Video URL:\n")
-    download_location = input("Enter download location:\n")
+    link = input("Enter YouTube Video URL: ")
+    download_location = input("Enter download location: ")
     if link and download_location:
         try:
             os.system(
                 f'yt-dlp -f best -o "{download_location}/%(title)s.%(ext)s" "{link}" -S res:1080'
             )
-            messagebox.showinfo("Success", "Download completed!")
+            print("Success", "Download completed!")
         except Exception as e:
-            messagebox.showerror("Error", f"An error occurred: {e}")
+            print("Error", f"An error occurred: {e}")
     else:
-        messagebox.showerror(
+        print(
             "Error", "Please provide a valid YouTube URL and download location."
         )
 
-
 def convert_file():
     try:
-        finput = input("Enter the input file path:\n")
-        foutput = input("Enter the output file path:\n")
+        finput = input("Enter the input file path: ")
+        foutput = input("Enter the output file path: ")
         (ffmpeg.input(finput).output(foutput).run())
-        messagebox.showinfo(message="Conversion Complete")
         print("Conversion Complete")
     except Exception as e:
-        messagebox.showerror(message=f"{e}")
+        print(f"{e}")
