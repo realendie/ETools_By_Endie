af2ee6a7d84e5b1a1113bc8f15819375654bf03f
realendie <enderproplays54@gmail.com>
2025-06-22 14:08:16 -0400

    Corrected file download formatting to best

--- ETools/download_video.py
+++ ETools/download_video.py
@@ -1,18 +1,14 @@
 import os
 import ffmpeg
 
-#def download_video():
+# def download_video():
 link = input("Enter YouTube Video URL: ")
 download_location = input("Enter download location: ")
 if link and download_location:
     try:
-        os.system(
-            f'yt-dlp -f best -o "{download_location}/%(title)s.%(ext)s" "{link}" -S res:1080'
-        )
+        os.system(f'yt-dlp -o "{download_location}/%(title)s.%(ext)s" "{link}"')
         print("Success", "Download completed!")
     except Exception as e:
         print("Error", f"An error occurred: {e}")
 else:
-    print(
-        "Error", "Please provide a valid YouTube URL and download location."
-    )
+    print("Error: Please provide a valid YouTube URL and download location.")
