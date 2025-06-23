7768f871393a853cc0d0e8111df30c22451d1d85
realendie <enderproplays54@gmail.com>
2025-06-23 01:41:47 -0400

    Removed testing functionality

--- ETools/download_video.py
+++ ETools/download_video.py
@@ -1,14 +1,14 @@
 import os
-import ffmpeg
 
-# def download_video():
-link = input("Enter YouTube Video URL: ")
-download_location = input("Enter download location: ")
-if link and download_location:
-    try:
-        os.system(f'yt-dlp -o "{download_location}/%(title)s.%(ext)s" "{link}"')
-        print("Success", "Download completed!")
-    except Exception as e:
-        print("Error", f"An error occurred: {e}")
-else:
-    print("Error: Please provide a valid YouTube URL and download location.")
+
+def download_video():
+    link = input("Enter YouTube Video URL: ")
+    download_location = input("Enter download location: ")
+    if link and download_location:
+        try:
+            os.system(f'yt-dlp -o "{download_location}/%(title)s.%(ext)s" "{link}"')
+            print("Success", "Download completed!")
+        except Exception as e:
+            print("Error", f"An error occurred: {e}")
+    else:
+        print("Error: Please provide a valid YouTube URL and download location.")
