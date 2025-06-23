225a4e6d5cd2cd4af56573830b6563858b9906d0
realendie <enderproplays54@gmail.com>
2025-06-23 16:47:19 -0400

    Made `convert_file.py` a function instead of raw text.

--- ETools/convert_file.py
+++ ETools/convert_file.py
@@ -5,13 +5,15 @@ import ffmpeg
 # YELLOW TEXT = \033[33m
 # RESET TEXT COLOR = \033[0m
 
-try:
-    print(
-        "\033[31mREMINDER:\033[33m File paths with spaces should be enclosed in double quotes.\033[0m"
-    )
-    finput = input("Enter the input file path: ")
-    foutput = input("Enter the output file path: ")
-    os.system(f"ffmpeg -i {finput} {foutput}")
-    print("Conversion Complete")
-except Exception as e:
-    print(f"{e}")
+
+def convert_file():
+    try:
+        print(
+            "\033[31mREMINDER:\033[33m File paths with spaces should be enclosed in double quotes.\033[0m"
+        )
+        finput = input("Enter the input file path: ")
+        foutput = input("Enter the output file path: ")
+        os.system(f"ffmpeg -i {finput} {foutput}")
+        print("Conversion Complete")
+    except Exception as e:
+        print(f"{e}")
